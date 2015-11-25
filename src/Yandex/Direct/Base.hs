f7ce465dd92ae4369a7c474d156514870fb96038
dpetrov <dpetrov@mabius.ru>
2015-11-25 15:00:42 +0300

    Add error handling

--- src/Yandex/Direct/Base.hs
+++ src/Yandex/Direct/Base.hs
@@ -1,40 +1,41 @@
+{-# LANGUAGE DeriveGeneric     #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE DeriveGeneric #-}
 
 
 module Yandex.Direct.Base (ydpost) where
 
-import Control.Lens ((^.))
-import Data.Aeson
-       (FromJSON, ToJSON, toJSON, object, (.=), parseJSON,
-        FromJSON, (.:), Value(Object))
+import           Control.Lens        ((^.))
+import           Data.Aeson          (FromJSON, FromJSON, ToJSON,
+                                      Value (Object), object, parseJSON, toJSON,
+                                      (.:), (.=))
 
-import GHC.Generics (Generic)
-import Data.Aeson.Lens (key)
-import Data.Text (Text)
-import Network.Wreq (asJSON, post, responseBody, Response)
-import Control.Exception (throwIO)
-import Control.Monad (mzero)
+import           Control.Applicative ((<|>))
+import           Control.Exception   (throwIO)
+import           Control.Monad       (mzero)
+import           Data.Aeson.Lens     (key)
+import           Data.Text           (Text)
+import           GHC.Generics        (Generic)
+import           Network.Wreq        (Response, asJSON, post, responseBody)
 
-import Yandex.Direct.Types
-import Debug.Trace
+import           Debug.Trace
+import           Yandex.Direct.Types
 
 
 data GenYDReq a = GenYDReq
     { method :: Method
-    , param :: Maybe a
+    , param  :: Maybe a
     , locale :: Text
-    , token :: Token
+    , token  :: Token
     } deriving (Generic)
 
 instance ToJSON a => ToJSON (GenYDReq a)
 
 data GenYDRep a = GenYDRep
     { _data :: a
-    } deriving (Show)
+    } | GenYDErr { error_code :: Int, error_str :: Text, error_detail :: Text} deriving (Show)
 
 instance FromJSON a => FromJSON (GenYDRep a) where
-    parseJSON (Object v) = GenYDRep <$> v .: "data"
+    parseJSON (Object v) = (GenYDRep <$> v .: "data") <|> (GenYDErr <$> v .: "error_code" <*> v .: "error_str" <*> v .: "error_detail")
     parseJSON _ = mzero
 
 ydpost
@@ -49,9 +50,11 @@ ydpost m p t =
              , param = p
              , token = t
              , locale = "ru"
-             }) >>= \r -> trace (show r) $ 
+             }) >>= \r -> trace (show r) $
     asJSON r >>=
     extractData
   where
     extractData :: (Show b) => Response (GenYDRep b) -> IO b
-    extractData r = return $ _data (r ^. responseBody)
+    extractData r = case r ^. responseBody of
+                      (GenYDRep d) -> return d
+                      (GenYDErr code str detail) -> throwIO $ ydException code str detail
