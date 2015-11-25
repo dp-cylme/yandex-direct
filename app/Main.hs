460b17adff590d9e9d5af65b4879fb4e4430eb13
dpetrov <dpetrov@mabius.ru>
2015-11-25 15:34:03 +0300

    Example of use library

--- app/Main.hs
+++ app/Main.hs
@@ -7,7 +7,7 @@ import Yandex
 
 main :: IO ()
 main = do
-    let token = "5c8094a1020b47fbbfe315b428c3cea6" :: Text
+    let token = "{your-token}" :: Text
         phrases = ["пух -винни", "синтепон"] :: [Text]
         geoids = ["213"]
     reportId <- createNew token phrases geoids
